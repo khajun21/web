bf1c8d882890f5813afaf0dcaf2de9b5729b0a24
Danh <danhzxc555@gmail.com>
2025-07-31 19:47:53 +0700

    commit

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
-const express = require('express')
+require("dotenv").config();
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.send("Hello, World ");
 });
 
 app.listen(port, () => {
